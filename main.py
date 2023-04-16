444642fb1b953a80dc7fa9b9ae047d35d0e9bb98
ScureX <multiaccshop@web.de>
2023-04-16 22:55:28 +0200

    set vars after loading

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ import json
 import os
 from dotenv import load_dotenv
 
-# Cloudflare API credentials
-ZONE_ID = os.getenv("ZONE_ID")
-API_TOKEN = os.getenv("API_TOKEN")
-RECORDS = json.loads(os.environ.get("RECORDS"))
-
-# URL to check
-URL = "http://{}".format(next(iter(RECORDS)))
-
-# Cloudflare API endpoint for updating a DNS record
-API_URL = "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}"
-
 
 def get_current_ip():
     # Retrieve the current public IP address
@@ -25,7 +14,7 @@ def get_current_ip():
         return None
 
 
-def update_dns_record(ip, record_name):
+def update_dns_record(ip, record_name, API_URL, API_TOKEN, ZONE_ID, RECORDS):
     # Update the DNS record with the new IP address
     url = API_URL.format(ZONE_ID, RECORDS[record_name])
     headers = {
@@ -43,20 +32,36 @@ def update_dns_record(ip, record_name):
     response.raise_for_status()
 
 
-def check_domain_status():
+def check_domain_status(URL):
     # Check whether the domain is properly responding
     response = requests.get(URL)
     return response.ok
 
 
+def load_vars():
+    # Cloudflare API credentials
+    ZONE_ID = os.getenv("ZONE_ID")
+    API_TOKEN = os.getenv("API_TOKEN")
+    RECORDS = json.loads(os.getenv("RECORDS"))
+
+    # URL to check
+    URL = "http://{}".format(next(iter(RECORDS)))
+
+    # Cloudflare API endpoint for updating a DNS record
+    API_URL = "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}"
+    return ZONE_ID, API_TOKEN, RECORDS, URL, API_URL
+
+
 def main():
     # Load environment variables from .env file
-    load_dotenv()
+    load_dotenv(os.getcwd() + "/.env")
     # add logging
     logging.basicConfig(filename='CloudflareIPChanger.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
+    ZONE_ID, API_TOKEN, RECORDS, URL, API_URL = load_vars()
+
     # Check the domain status
-    if not check_domain_status():
+    if not check_domain_status(URL):
         # If the domain is not responding, update the DNS record
         ip = get_current_ip()
 
@@ -66,7 +71,7 @@ def main():
             return
         
         for record_name in RECORDS:
-            update_dns_record(ip, record_name)
+            update_dns_record(ip, record_name, API_URL, API_TOKEN, ZONE_ID, RECORDS)
 
         logging.warning("Updated DNS record with new IP address: {}".format(ip))
     else:
